eddb69f9ee64c712a94d571e511790f3deacc995
Jonas Andersen <github@jonas.anonaddy.com>
2024-01-26 15:28:15 +0100

    define draft of game board's API

--- src/main/scala/ti4/model/GameBoard.scala
+++ src/main/scala/ti4/model/GameBoard.scala
@@ -1,27 +1,36 @@
 package ti4.model
 
+import ti4.model.Unit.Ship
+
 import scala.collection.immutable.Map as SMap
 import scala.collection.mutable.ArrayBuffer
 
 object GameBoard {
 
-  private type TileId = String
-  private type Weight = Int
+  private type TileId     = String
+  private type Weight     = Int
   private type Neighbours = SMap[TileId, Weight]
-  opaque type HexTile = (TileId, Neighbours)
-  opaque type Grid = SMap[TileId, Neighbours]
-  opaque type Tiles = SMap[TileId, GameBoardTile.Tile]
+  opaque type HexTile     = (TileId, Neighbours)
+  opaque type Grid        = SMap[TileId, Neighbours]
+  opaque type Tiles       = SMap[TileId, GameBoardTile.Tile]
   // option to is to ensure that is not empty as not all tiles will be set to make a valid hex board
   opaque type Layout = Array[Array[GameBoardTile.Tile]]
+  trait Feature
+  final case class Path(targetTile: TileId, subsequentTiles: List[TileId])
+  final case class ShipPath(ship: Ship, path: Path)
 
-  ArrayBuffer
-  Vector
+//  ArrayBuffer
+//  Vector
 
-  // what are my use cases?
+  // Where to verify that movement is valid?
+  def move(targetTile: TileId, ships: List[Ship]): Tile = ???
 
-  // Think of them and write them out
-  // tiles can be everywhere on the board, a tile id does not tell us anything of the layout
-
-  //
+  /**
+   * Returns the ships that can reach the target tile. The board will store ship's id and the tile it is on. So we know how far it needs to move.
+   * @param targetTile destination tile
+   * @param ships should know how much movement they have
+   * @param features things that might change the movement of the ships
+   */
+  def shipsInRange(targetTile: TileId, ships: List[Ship], features: List[Feature]): List[ShipPath] = List.empty
 
 }
