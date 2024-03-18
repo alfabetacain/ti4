8a109e05c52b82c607d2fabdb90b364d47b61352
Jonas Andersen <github@jonas.anonaddy.com>
2024-03-18 16:58:27 +0100

    format

--- domain/src/test/scala/ti4/persistence/GameBoardSetupRepositoryTest.scala
+++ domain/src/test/scala/ti4/persistence/GameBoardSetupRepositoryTest.scala
@@ -1,8 +1,8 @@
 package ti4.persistence
 
-import ti4.collection.{Hex, HexGrid}
+import ti4.collection.{ Hex, HexGrid }
 import ti4.identifiers.GameBoardSetupId
-import ti4.model.{GameBoardSetup, Tile}
+import ti4.model.{ GameBoardSetup, Tile }
 
 class GameBoardSetupRepositoryTest extends munit.FunSuite {
 
@@ -12,15 +12,15 @@ class GameBoardSetupRepositoryTest extends munit.FunSuite {
   }
 
   test("retrieving a valid setup should be successful") {
-    val topLeftCorner = Set(Hex(-3, 1), Hex(-3, 0), Hex(-2, -1))
+    val topLeftCorner  = Set(Hex(-3, 1), Hex(-3, 0), Hex(-2, -1))
     val topRightCorner = Set(Hex(2, -3), Hex(3, -3), Hex(3, -2))
-    val bottom = Set(Hex(-1, 3), Hex(0, 3), Hex(1, 2))
-    val extraTiles = (topLeftCorner ++ topRightCorner ++ bottom).map(hex => hex -> Tile.Empty).toSeq
-    val id = makeId("ti4-game-board-setup-3p-0")
+    val bottom         = Set(Hex(-1, 3), Hex(0, 3), Hex(1, 2))
+    val extraTiles     = (topLeftCorner ++ topRightCorner ++ bottom).map(hex => hex -> Tile.Empty).toSeq
+    val id             = makeId("ti4-game-board-setup-3p-0")
     val expected = GameBoardSetup(
       id = id,
       name = "Three-Player Setup",
-      grid = HexGrid.generate(2, _ => Tile.Empty).withHexes(extraTiles:_*),
+      grid = HexGrid.generate(2, _ => Tile.Empty).withHexes(extraTiles: _*),
       homePositions = Set(Hex(-3, 0), Hex(3, -3), Hex(0, 3))
     )
     val result = GameBoardSetupRepository.getGameBoardSetup(id)
@@ -28,6 +28,7 @@ class GameBoardSetupRepositoryTest extends munit.FunSuite {
     assertEquals(result, Some(expected))
   }
 
-  private def makeId(id: String) = GameBoardSetupId(id).toOption.getOrElse(fail(s"given invalid game board setup id '$id'"))
+  private def makeId(id: String) =
+    GameBoardSetupId(id).toOption.getOrElse(fail(s"given invalid game board setup id '$id'"))
 
 }
